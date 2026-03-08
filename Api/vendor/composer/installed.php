a56ff18fa1e80baa5066e95131efe8e4ce2a1dca
Roberto Zúñiga Altamirano <carlosroart13@gmail.com>
2026-03-07 22:56:03 -0600

    Refactor Form component to improve validation and submission processes, add reset functionality, and update index.html to link the new JavaScript asset. Remove obsolete JavaScript file to enhance build efficiency.

---
+++ Api/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'viajerocr/api',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '67d085fe45099876bc758697657d2bad2a2b76ce',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.12.0',
+            'version' => '6.12.0.0',
+            'reference' => 'd1ac35d784bf9f5e61b424901d5a014967f15b12',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'viajerocr/api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '67d085fe45099876bc758697657d2bad2a2b76ce',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
